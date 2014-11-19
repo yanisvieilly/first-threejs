e8fc25d4af88eadd3d7c3223e032fba440ecf555
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-11-19 02:06:13 +0100

    Add coffee task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
 
 gulp.task('default', function() {
 
 });
+
+gulp.task('coffee', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(coffee({ bare: true }).on('error', gutil.log))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.watch('./src/*.coffee', ['coffee']);
