e2b6f1e5b8546be64fa03cb95ffac00088dad16e
Yanis Vieilly <yanis.vieilly@gmail.com>
2014-11-19 02:19:19 +0100

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 
-gulp.task('default', function() {
-
-});
-
 gulp.task('coffee', function() {
-  gulp.src('./src/*.coffee')
+  gulp.src('src/*.coffee')
     .pipe(coffee({ bare: true }).on('error', gutil.log))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('dist'))
 });
 
-gulp.watch('./src/*.coffee', ['coffee']);
+gulp.task('watch', function() {
+  gulp.watch('src/*.coffee', ['coffee']);
+});
+
+gulp.task('default', ['watch']);
